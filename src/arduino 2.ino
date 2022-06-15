d9fce69356793dee4eb8fe6c535ddcc678c00f0b
Vlad <guslitge@student.gu.se>
2022-06-15 16:10:15 +0200

    Add wifi connectivity

--- src/arduino 2.ino
+++ src/arduino 2.ino
@@ -1,6 +1,9 @@
 #include "DHT.h"
 #include <TM1637.h>
 #include <math.h>
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include "arduino_secrets.h"
 
 #define DHTPIN 4
 #define MOTIONPIN 3
@@ -19,6 +22,10 @@
 DHT dht(DHTPIN, DHTTYPE);
 TM1637 tm(DISPLAYCLK, DISPLAYDIO);
 
+char ssid[] = SECRET_SSID;   // your network SSID (name)
+char pass[] = SECRET_PASS;   // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS; // the Wifi radio's status
+
 void setup()
 {
     Serial.begin(9600);
@@ -32,6 +39,20 @@ void setup()
     pinMode(GREENLED, OUTPUT);
     dht.begin();
     tm.begin();
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED)
+    {
+        Serial.print("Attempting to connect to WPA SSID: ");
+        Serial.println(ssid);
+        // Connect to WPA/WPA2 network:
+        status = WiFi.begin(ssid, pass);
+        // wait 10 seconds for connection:
+        delay(10000);
+    }
+    // you're connected now, so print out the data:
+    Serial.print("You're connected to the network");
+    printCurrentNet();
+    printWifiData();
 }
 
 void loop()
@@ -154,4 +175,56 @@ void controlRGBLED(int red, int green, int blue)
 void controlDisplay(String str)
 {
     tm.display(str);
+}
+
+void printWifiData()
+{
+    // print your board's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+    Serial.println(ip);
+    // print your MAC address:
+    byte mac[6];
+    WiFi.macAddress(mac);
+    Serial.print("MAC address: ");
+    printMacAddress(mac);
+}
+
+void printCurrentNet()
+{
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+    // print the MAC address of the router you're attached to:
+    byte bssid[6];
+    WiFi.BSSID(bssid);
+    Serial.print("BSSID: ");
+    printMacAddress(bssid);
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.println(rssi);
+    // print the encryption type:
+    byte encryption = WiFi.encryptionType();
+    Serial.print("Encryption Type:");
+    Serial.println(encryption, HEX);
+    Serial.println();
+}
+
+void printMacAddress(byte mac[])
+{
+    for (int i = 5; i >= 0; i--)
+    {
+        if (mac[i] < 16)
+        {
+            Serial.print("0");
+        }
+        Serial.print(mac[i], HEX);
+        if (i > 0)
+        {
+            Serial.print(":");
+        }
+    }
+    Serial.println();
 }
